efc3e7a19e7aaacf83e7f4809a16b0ebdeebd6b2
ExtraMobs <extramobs@hotmail.com>
2026-08-26 09:22:39 -0300

    align libp2p protocol interfaces

--- packages/transpiled_libp2p/lib/src/core/protocol/protocol.dart
+++ packages/transpiled_libp2p/lib/src/core/protocol/protocol.dart
@@ -3,20 +3,27 @@
 /// Core protocol identifiers and negotiation interfaces.
 typedef ProtocolId = String;
 
-List<ProtocolId> convertFromStrings(Iterable<String> ids) => List.unmodifiable(ids);
-List<String> convertToStrings(Iterable<ProtocolId> ids) => List.unmodifiable(ids);
+List<ProtocolId> convertFromStrings(Iterable<String> ids) =>
+    List.unmodifiable(ids);
+List<String> convertToStrings(Iterable<ProtocolId> ids) =>
+    List.unmodifiable(ids);
 
-typedef HandlerFunc = Future<void> Function(Object stream, ProtocolId protocol);
+typedef HandlerFunc = Future<void> Function(ProtocolId protocol, Object stream);
 
 abstract interface class Router {
   void addHandler(ProtocolId id, HandlerFunc handler);
+  void addHandlerWithFunc(
+    ProtocolId id,
+    bool Function(ProtocolId) match,
+    HandlerFunc handler,
+  );
   void removeHandler(ProtocolId id);
+  List<ProtocolId> protocols();
 }
 
 abstract interface class Negotiator {
-  Future<(Object, ProtocolId)> negotiate(Object stream, List<ProtocolId> protocols);
+  Future<(ProtocolId, HandlerFunc)> negotiate(Object stream);
+  Future<void> handle(Object stream);
 }
 
-abstract interface class ProtocolSwitch implements Router, Negotiator {
-  List<ProtocolId> protocols();
-}
+abstract interface class ProtocolSwitch implements Router, Negotiator {}
